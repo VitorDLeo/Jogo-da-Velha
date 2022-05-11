5a93f094dcf2ef356c36b8e45545aec85fd4c733
VitorDLeo <vitordileo003@gmail.com>
2022-05-11 15:17:53 -0300

    Corrigindo e finalizando o Projeto

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,17 +1,56 @@
 // iniciar as variaveis
 let board = ['','','','','','','','',''];
 let playerTime = 0;
-let symbols = ['o', 'x']
+let symbols = ['o', 'x'];
+let gameOver = false;
+
 
 function handleMove(position){
+
+    if (gameOver){
+        return;
+    }
 
     if (board[position] == ''){
         board[position] = symbols[playerTime];
 
-        if (playerTime == 0){
-            playerTime = 1;
+        gameOver = isWin()
+
+        if (gameOver == false){
+            if (playerTime == 0){
+                playerTime = 1;
+            } else {
+                playerTime = 0;
+            }
+        }
+    }
+}
+
+function isWin(){
+
+    let winState = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+
+    for(let i = 0; i<winState.length; i++){
+
+        let seq = winState[i];
+
+        let pos1 = seq[0];
+        let pos2 = seq[1];
+        let pos3 = seq[2];
+
+        if(board[pos1] == board[pos2] && board[pos1]== board[pos3] && board[pos1] != ''){
+            return true;
         } else {
-            playerTime = 0;
+            return false;
         }
     }
 }
